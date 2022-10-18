fe2b2117f3b41d7fed61521c56af53715415e73c
Finn Pedersen <finnpedersenfrance@gmail.com>
2022-10-18 11:23:44 +0200

    Added types

--- elm-s05l02-my-dog-colors/src/Main.elm
+++ elm-s05l02-my-dog-colors/src/Main.elm
@@ -2,14 +2,20 @@ module Main exposing (..)
 
 import Element
 import Element.Font
+import Html exposing (Html)
 
+main : Html msg
 main =
     viewLayout
 
+red : Element.Color
 red = Element.rgb255 255 0 0
+blue : Element.Color
 blue = Element.rgb255 0 0 200
+black : Element.Color
 black = Element.rgb255 0 0 0
 
+viewLayout : Html msg
 viewLayout =
     Element.layoutWith {
         options = []
@@ -22,6 +28,7 @@ viewLayout =
         , viewParagraph
     ])
 
+viewParagraph : Element.Element msg
 viewParagraph =
     Element.paragraph [ Element.Font.color black ]
     [
